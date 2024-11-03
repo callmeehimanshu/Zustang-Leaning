922deeb580507feebcc07a8fb74d1a8195838097
Himanshu <himanshuagl18@gmail.com>
2024-11-03 18:15:28 +0530

    react setup with TS

---
+++ my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+     hello
+     
+     
+    </div>
+  );
+}
+
+export default App;
